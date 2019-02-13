87aca38e23e88f989ce1baec6a8cc35c0dd9c2d5
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-13 19:42:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
-
+def my_collect (collection)
+  array = []
+  if block_given?
+    i = 0
+    while i < collection.length
+      new_ele = yield(collection[i])
+      array << new_ele
+      i = i + 1
+    end
+    return array.upcase
+    
+  end 
+end
