95971e103bacea010851464955a78e9012363302
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-13 19:49:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,9 +7,10 @@ def my_collect (collection)
     while i < collection.length
       new_ele = yield(collection[i])
       array << new_ele.upcase
-      other_array << new_ele.
+      other_array << new_ele.split(" ").first
       i = i + 1
     end
     return array
+    return other_array
   end 
 end
